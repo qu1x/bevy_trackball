b3e1ac317e2ed888b58a0c040a2e7a3c28d1dda3
Rouven Spreckels <rs@qu1x.dev>
2023-11-06 20:54:21 +0100

    Update `egui` example.

--- examples/egui.rs
+++ examples/egui.rs
@@ -198,7 +198,10 @@ fn render_to_image_example_system(
 	let ctx = contexts.ctx_mut();
 	let mut apply = false;
 	egui::Window::new("Cube material preview").show(ctx, |ui| {
-		ui.image(cube_preview_texture_id, [300.0, 300.0]);
+		ui.image(egui::load::SizedTexture::new(
+			cube_preview_texture_id,
+			egui::vec2(300.0, 300.0),
+		));
 		egui::Grid::new("preview").show(ui, |ui| {
 			ui.label("Base color:");
 			color_picker_widget(ui, &mut preview_material.base_color);
@@ -228,7 +231,7 @@ fn render_to_image_example_system(
 		let material_clone = preview_material.clone();
 
 		let main_material_handle = main_cube_query.single();
-		let _ = materials.set(main_material_handle, material_clone);
+		materials.insert(main_material_handle, material_clone);
 	}
 }
 
