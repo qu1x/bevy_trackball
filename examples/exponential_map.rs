c6531af5964d65be70739242c0f8f00f4a3d47c3
Rouven Spreckels <rs@qu1x.dev>
2025-02-22 20:24:10 +0100

    Avoid ambiguous Unicode character.

--- examples/exponential_map.rs
+++ examples/exponential_map.rs
@@ -3,13 +3,13 @@
 //!
 //! This is an alternative trackball technique using exponential map and parallel transport to
 //! preserve distances and angles for inducing coherent and intuitive trackball rotations. For
-//! instance, displacements on straight radial lines through the screen’s center are carried to arcs
+//! instance, displacements on straight radial lines through the screen's center are carried to arcs
 //! of the same length on great circles of the trackball (e.g., dragging the mouse along an eights
 //! of the trackball's circumference rolls the camera by 360/8=45 degrees, dragging the mouse from
 //! the screen's center to its further edge *linearly* rotates the camera by 1 [radian], where the
 //! trackball's diameter is the maximum of the screen's width and height). This is in contrast to
 //! state-of-the-art techniques using orthogonal projection which distorts radial distances further
-//! away from the screen’s center (e.g., the rotation accelerates towards the edge).
+//! away from the screen's center (e.g., the rotation accelerates towards the edge).
 //!
 //! [radian]: https://en.wikipedia.org/wiki/Radian
 
